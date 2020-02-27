453fa3b74e227f52ca8e1ef0f44089b454bb6ce1
Michu <maximichu777@gmail.com>
2020-02-27 21:27:35 +0100

    Segundo commit de acepta el reto.

--- src/acepta_el_reto/_174AñoNuevo.java
+++ src/acepta_el_reto/_174AñoNuevo.java
@@ -1,5 +1,48 @@
 package acepta_el_reto;
 
-public class _174AñoNuevo {
+import java.util.Scanner;
 
+public class _174AñoNuevo {
+	public static void main(String[] args) {
+
+		Scanner tec = new Scanner(System.in);
+		int numero = tec.nextInt();
+
+		boolean repetido = repetidos(String.valueOf(numero)), repDeNuevo = repetido;
+		int signumero = numero, anyoMenor = -1;
+
+		while (repDeNuevo == repetido) {
+			repDeNuevo = repetidos(String.valueOf(signumero));
+			if (repDeNuevo == repetido) {
+				signumero--;
+				anyoMenor++;
+			}
+		}
+
+		signumero = numero;
+		repDeNuevo = repetido;
+		int anyoMayor = 0;
+
+		while (repDeNuevo == repetido) {
+			repDeNuevo = repetidos(String.valueOf(signumero));
+			if (repDeNuevo == repetido) {
+				signumero++;
+				anyoMayor++;
+			}
+		}
+		int Anyo1 = numero - anyoMenor;
+		int seriemayor = anyoMayor + anyoMenor;
+		System.out.println(Anyo1 + " " + seriemayor);
+	}
+
+	public static boolean repetidos(String num) {
+		for (int i = 0; i < num.length() - 1; i++) {
+			for (int j = i + 1; j < num.length(); j++) {
+				if (num.charAt(i) == num.charAt(j)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
